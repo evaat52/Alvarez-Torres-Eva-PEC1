429c437f57154ba302bda28f08ea349425b85682
Eva Alvarez <eva.at91@gmail.com>
2025-04-02 00:53:54 +0200

    crear objeto SumarizedExperiment

--- Rscript.R
+++ Rscript.R
@@ -30,3 +30,38 @@ str(metabolomics_data)
 head(metabolomics_data)
 # Verifica el número de filas y columnas
 dim(metabolomics_data) 
+
+# 3. Creación del SummarizedExperiment Object
+
+# Extraer la información de los pacientes
+colData <- metabolomics_data[, 1:6]  # Variables cualitativas
+colData <- DataFrame(colData)  # Convertir a DataFrame de Bioconductor
+dim(colData)  
+head(colData)
+
+# Extraer la matriz de datos (variables cuantitativas)
+assay_data <- as.matrix(metabolomics_data[, 7:ncol(metabolomics_data)])
+dim(assay_data)  
+head(assay_data[, 1:5])  # Para ver los primeros valores
+
+# Transponer la matriz de datos para que las muestras estén en filas
+assay_data <- t(assay_data)
+
+# Verificar dimensiones después de la transposición
+dim(assay_data)  
+
+#  SummarizedExperiment
+se <- SummarizedExperiment(
+  assays = list(counts = assay_data),
+  colData = colData
+)
+se
+
+# Ver la estructura del objeto SummarizedExperiment
+str(se)
+
+# Ver la estructura de los metadatos del objeto SummarizedExperiment
+str(colData(se))
+
+# Ver la estructura de colData directamente
+colData(se)
